967aca1ae71974f1d237668c41c6bf37d0bb3fa0
RomanZ <geralt1530@gmail.com>
2020-11-24 21:00:35 +0200

    changed viewpoint

--- CollFilt.py
+++ CollFilt.py
@@ -5,36 +5,36 @@ import numpy as np
 from SimScore import pearson_score
 
 def build_arg_parser():
-    parser = argparse.ArgumentParser(description='Find users who are similar to the input user')
-    parser.add_argument('--user', dest='user', required=True,
-            help='Input user')
+    parser = argparse.ArgumentParser(description='Find projects who are similar to the input project')
+    parser.add_argument('--project', dest='project', required=True,
+            help='Input project')
     return parser
  
-def find_similar_users(dataset, user, num_users):
-    if user not in dataset:
-        raise TypeError('Cannot find ' + user + ' in the dataset')
+def find_similar_projects(dataset, project, num_projects):
+    if project not in dataset:
+        raise TypeError('Cannot find ' + project + ' in the dataset')
 
-    scores = np.array([[x, pearson_score(dataset, user, 
-            x)] for x in dataset if x != user])
+    scores = np.array([[x, pearson_score(dataset, project, 
+            x)] for x in dataset if x != project])
 
     scores_sorted = np.argsort(scores[:, 1])[::-1]
 
-    top_users = scores_sorted[:num_users] 
+    top_projects = scores_sorted[:num_projects] 
 
-    return scores[top_users] 
+    return scores[top_projects] 
 
 if __name__=='__main__':
     args = build_arg_parser().parse_args()
-    user = args.user
+    project = args.project
 
     ratings_file = 'ratings.json'
 
     with open(ratings_file, 'r') as f:
         data = json.loads(f.read())
 
-    print('\nUsers similar to ' + user + ':\n')
-    similar_users = find_similar_users(data, user, 3) 
-    print('User\t\t\tSimilarity score')
+    print('\nprojects similar to ' + project + ':\n')
+    similar_projects = find_similar_projects(data, project, 3) 
+    print('project\t\t\tSimilarity score')
     print('-'*41)
-    for item in similar_users:
+    for item in similar_projects:
         print(item[0], '\t\t', round(float(item[1]), 2))
--- RecSys.py
+++ RecSys.py
@@ -3,32 +3,32 @@ import json
 import numpy as np
 
 from SimScore import pearson_score
-from CollFilt import find_similar_users
+from CollFilt import find_similar_projects
 
 def build_arg_parser():
-    parser = argparse.ArgumentParser(description='Find the movie recommendations for the given user')
-    parser.add_argument('--user', dest='user', required=True,
-            help='Input user')
+    parser = argparse.ArgumentParser(description='Find the employee recommendations for the given project')
+    parser.add_argument('--project', dest='project', required=True,
+            help='Input project')
     return parser
  
-def get_recommendations(dataset, input_user):
-    if input_user not in dataset:
-        raise TypeError('Cannot find ' + input_user + ' in the dataset')
+def get_recommendations(dataset, input_project):
+    if input_project not in dataset:
+        raise TypeError('Cannot find ' + input_project + ' in the dataset')
 
     overall_scores = {}
     similarity_scores = {}
 
-    for user in [x for x in dataset if x != input_user]:
-        similarity_score = pearson_score(dataset, input_user, user)
+    for project in [x for x in dataset if x != input_project]:
+        similarity_score = pearson_score(dataset, input_project, project)
 
         if similarity_score <= 0:
             continue
         
-        filtered_list = [x for x in dataset[user] if x not in \
-                dataset[input_user] or dataset[input_user][x] == 0]
+        filtered_list = [x for x in dataset[project] if x not in \
+                dataset[input_project] or dataset[input_project][x] == 0]
 
         for item in filtered_list: 
-            overall_scores.update({item: dataset[user][item] * similarity_score})
+            overall_scores.update({item: dataset[project][item] * similarity_score})
             similarity_scores.update({item: similarity_score})
 
     if len(overall_scores) == 0:
@@ -45,14 +45,14 @@ def get_recommendations(dataset, input_user):
  
 if __name__=='__main__':
     args = build_arg_parser().parse_args()
-    user = args.user
+    project = args.project
 
     ratings_file = 'ratings.json'
 
     with open(ratings_file, 'r') as f:
         data = json.loads(f.read())
 
-    print("\nMovie recommendations for " + user + ":")
-    movies = get_recommendations(data, user) 
+    print("\Employee recommendations for " + project + ":")
+    movies = get_recommendations(data, project) 
     for i, movie in enumerate(movies):
         print(str(i+1) + '. ' + movie)
